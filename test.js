7a33b3fa3f69f2e58b2dbe63b94387ebd7c63b23
SamHnnz <hlsamuel0@gmail.com>
2020-12-05 14:34:37 +0000

    Excuse Generator 2.0

--- test.js
+++ test.js
@@ -1,15 +1,17 @@
-let who = ['The dog','My grandma','His turtle','My bird'];
-let action = ['ate','peed','crushed','broke'];
-let what = ['my homework', 'the keys', 'the car'];
-let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
 
-let whoRandom = who[Math.floor(Math.random() * 4)];
-let actionRandom = action[Math.floor(Math.random() * 4)];
-let whatRandom = what[Math.floor(Math.random() * 3)];
-let whenRandom = when[Math.floor(Math.random() * 5)];
-
-function getRandomExcuse (reason1,reason2,reason3,reason4) {
-    return reason1+" "+reason2+" "+reason3+" "+reason4;
+let getRandomExcuse = function(){
+  //Array with the words which will be used
+    let who = ['The dog ','My grandma ','His turtle ','My bird '];
+    let action = ['ate ','peed ','crushed ','broke '];
+    let what = ['my homework ', 'the keys ', 'the car '];
+    let when = ['before the class ','right on time ','when I finished ','during my lunch ','while I was praying '];
+  
+  // Random variables
+   let whoRandom = Math.floor((Math.random() * who.length) );
+   let actionRandom = Math.floor((Math.random() * action.length) );
+   let whatRandom = Math.floor((Math.random() * what.length) );
+   let whenRandom = Math.floor((Math.random() * when.length) );
+  
+ alert(who[whoRandom] + action[actionRandom] + what[whatRandom] + when[whenRandom]); 
+  
 }
-
-document.getElementById('excuses').innerHTML = getRandomExcuse(whoRandom,actionRandom,whatRandom,whenRandom);
